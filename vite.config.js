b1181cdf4524878c8f48bbb992172b5a7394bbd4
ephemeris-void <sakibulislamsair@gmail.com>
2026-06-06 02:09:44 +0000

    Production deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: '.' // এর ফলে বিল্ড ফাইলগুলো dist ফোল্ডারে না গিয়ে সরাসরি বাইরে তৈরি হবে
-  }
+  base: '/'
 })
